210fb3ee47078de2555c0f35807948cc67f11358
NoeliaNF <noe_nf90@yahoo.com.ar>
2017-06-30 20:33:33 -0300

    Primera ventana con jugadores

--- src/fiuba/algo3/eventos/BotonEntrarEventHandler.java
+++ src/fiuba/algo3/eventos/BotonEntrarEventHandler.java
@@ -1,18 +1,26 @@
 package fiuba.algo3.eventos;
 
+import fiuba.algo3.modelo.Juego.Jugador;
+import fiuba.algo3.modelo.Juego.JugadorEnemigo;
+import fiuba.algo3.modelo.Juego.JugadorZ;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class BotonEntrarEventHandler implements EventHandler<ActionEvent> {
 
     Stage stage;
     Scene proximaEscena;
 
-    public BotonEntrarEventHandler(Stage stage, Scene proximaEscena) {
+    public BotonEntrarEventHandler(Stage stage, Scene proximaEscena, String nombre1, String nombre2) {
         this.stage = stage;
         this.proximaEscena = proximaEscena;
+        JugadorZ jugadorZ = new JugadorZ(nombre1);
+        JugadorEnemigo jugadorEnemigo = new JugadorEnemigo(nombre2);
+
     }
 
     @Override
--- src/fiuba/algo3/vista/ContenedorBienvenidos.java
+++ src/fiuba/algo3/vista/ContenedorBienvenidos.java
@@ -7,10 +7,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ContenedorBienvenidos extends VBox {
@@ -32,21 +35,31 @@ public class ContenedorBienvenidos extends VBox {
 
         Button botonEntrar = new Button();
         botonEntrar.setText("Entrar");
-        Label etiquetaJugador = new Label();
-        etiquetaJugador.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
-        etiquetaJugador.setText("Nombre jugador Z : ");
+
+        Label etiquetaJugadorZ = new Label();
+        etiquetaJugadorZ.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
+        etiquetaJugadorZ.setText("Nombre jugador Z :            ");
         TextField texto = new TextField();
 
+        HBox layout = new HBox();
+        layout.getChildren().addAll( etiquetaJugadorZ, texto);
+
+        Label etiquetaJugadorEnemigo = new Label();
+        etiquetaJugadorEnemigo.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
+        etiquetaJugadorEnemigo.setText("Nombre jugador Enemigo: ");
+        TextField texto2 = new TextField();
+
+        HBox layout2 = new HBox();
+        layout2.getChildren().addAll( etiquetaJugadorEnemigo, texto2);
+
         Label etiqueta = new Label();
-        etiqueta.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
-
+        etiqueta.setFont(Font.font("Tahoma", FontWeight.BLACK, 18));
         etiqueta.setText("Elija jugadores ");
-        etiqueta.setTextFill(Color.web("#66A7C5"));
 
-        BotonEntrarEventHandler botonEntrarHandler = new BotonEntrarEventHandler(stage, proximaEscena);
+        BotonEntrarEventHandler botonEntrarHandler = new BotonEntrarEventHandler(stage, proximaEscena, texto.getPromptText(), texto2.getPromptText());
         botonEntrar.setOnAction(botonEntrarHandler);
 
-        this.getChildren().addAll(etiqueta,etiquetaJugador, texto, botonEntrar );
+        this.getChildren().addAll(etiqueta,layout, layout2, botonEntrar );
     }
 
 }
